8ae35b8a2e5b4c98d282edd4837bbb663b830889
Marat Khusnetdinov <marat@khusnetdinov.ru>
2025-07-13 16:00:04 +0300

    Sprint1:H

--- js/sprint1_nonfinals/h.js
+++ js/sprint1_nonfinals/h.js
@@ -13,8 +13,40 @@ _reader.on('line', line => {
 
 process.stdin.on('end', solve);
 
+// https://contest.yandex.ru/contest/22449/problems/H/?success=140117564#2989/2020_04_13/vTLkp8umJC
 function sumOfBinaries(firstNumber, secondNumber) {
     // Ваше решение
+    // console.log(`First = ${ firstNumber }, Second = ${ secondNumber }`);
+
+    let result = '';
+    let transfer = 0;
+    let firstIndex = firstNumber.length - 1;
+    let secondIndex = secondNumber.length - 1;
+
+    while (firstIndex >= 0 || secondIndex >= 0) {
+        // console.log(`FirstIndex= ${ firstIndex }, SecondIndex = ${ secondIndex }, Transfer ${ transfer }`);
+
+        const first = firstIndex >= 0 ? parseInt(firstNumber[firstIndex]) : 0;
+        const second = secondIndex >= 0 ? parseInt(secondNumber[secondIndex]) : 0;
+
+        // console.log(`First= ${ first }, SecondIndex = ${ second }, Transfer ${ transfer }`);
+
+        let sum = first + second + transfer;
+
+        result = (sum % 2).toString() + result;
+        transfer = Math.floor(sum / 2)
+
+        // console.log(`Sum= ${ sum }, Result = ${ result }, Transfer ${ transfer }`);
+
+        firstIndex -= 1;
+        secondIndex -= 1;
+    }
+
+    if (transfer > 0) {
+        result = transfer.toString() + result;
+    }
+
+    return result;
 }
 
 function solve() {
