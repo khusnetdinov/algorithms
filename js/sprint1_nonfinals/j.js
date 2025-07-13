97c84a58426704ae15288da31511e61d77e77544
Marat Khusnetdinov <marat@khusnetdinov.ru>
2025-07-13 21:04:23 +0300

    Sprint1:J

--- js/sprint1_nonfinals/j.js
+++ js/sprint1_nonfinals/j.js
@@ -13,8 +13,37 @@ _reader.on('line', line => {
 
 process.stdin.on('end', solve);
 
+// https://contest.yandex.ru/contest/22449/problems/J/?success=140126545#3484683/2020_11_16/FK4DlcVHux
 function factorize(number) {
     // Ваше решение
+    // Смотрим максимальное число для проверки
+    let maxFactor = number
+    let factors = [];
+
+    // Смотрим четные
+    while (number % 2 === 0) {
+        factors.push(2);
+        // console.log(`Factor = ${ 2 }`)
+
+        number = number / 2;
+    }
+
+    // Берем только нечетные
+    let oddDivisor = 3;
+    while (oddDivisor * oddDivisor < maxFactor) {
+        while (number % oddDivisor === 0) {
+            factors.push(oddDivisor);
+
+            // console.log(`Factor = ${ oddDivisor }`)
+
+            number = number / oddDivisor;
+        }
+        oddDivisor += 2;
+    }
+
+    if (number > 1) factors.push(number);
+
+    return factors
 }
 
 function solve() {
