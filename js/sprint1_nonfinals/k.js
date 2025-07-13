dde5e44c111bba3c2f68ad4b06c6e55190685997
Marat Khusnetdinov <marat@khusnetdinov.ru>
2025-07-13 22:49:43 +0300

    Sprint1:K

--- js/sprint1_nonfinals/k.js
+++ js/sprint1_nonfinals/k.js
@@ -13,8 +13,38 @@ _reader.on('line', line => {
 
 process.stdin.on('end', solve);
 
+// https://contest.yandex.ru/contest/22449/problems/K/?success=140129097#2989/2020_04_14/oiNq5SI4HQ
 function getSum(listNumber, number) {
     // Ваше решение
+    let result = [];
+    let transfer = 0;
+    let firstList = listNumber;
+    let secondList = number.toString().split('');
+    let firstIndex = firstList.length - 1;
+    let secondIndex = secondList.length - 1;
+
+    while (firstIndex >= 0 || secondIndex >= 0) {
+        const first = firstIndex >= 0 ? parseInt(firstList[firstIndex]) : 0;
+        const second = secondIndex >= 0 ? parseInt(secondList[secondIndex]) : 0;
+
+        // console.log(`First= ${ first }, SecondIndex = ${ second }, Transfer ${ transfer }`);
+
+        let sum = first + second + transfer;
+
+        result.unshift(sum % 10)
+        transfer = Math.floor(sum / 10);
+
+        // console.log(`Sum= ${ sum }, Result = ${ result }, Transfer ${ transfer }`);
+
+        firstIndex -= 1;
+        secondIndex -= 1;
+    }
+
+    if (transfer !== 0) {
+        result.unshift(transfer);
+    }
+
+    return result;
 }
 
 function solve() {
