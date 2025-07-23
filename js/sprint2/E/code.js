4a501afe459a99771ed794cb2b2f46b6b73e5f65
Marat Khusnetdinov <marat@khusnetdinov.ru>
2025-07-23 21:44:58 +0300

    Sprint2:E

--- js/sprint2/E/code.js
+++ js/sprint2/E/code.js
@@ -11,6 +11,24 @@ if (process.env.REMOTE_JUDGE !== 'true') {
 function solution(node) {
     // Your code
     // ヽ(´▽`)/
+    if (!node) return null
+
+    let currentNode = node
+    let newHead = node
+
+    // https://contest.yandex.ru/contest/22779/run-report/140419258/
+    while (currentNode) {
+        const originalNextNode = currentNode.next
+
+        currentNode.next = currentNode.prev
+        currentNode.prev = originalNextNode
+
+        newHead = currentNode
+
+        currentNode = originalNextNode
+    }
+
+    return newHead
 }
 
 function test() {
