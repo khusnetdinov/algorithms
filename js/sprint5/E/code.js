a6d76a94239c556fae39a1105b756a57faa53c76
Marat Khusnetdinov <marat@khusnetdinov.ru>
2025-09-08 23:00:51 +0300

    Sprint5:E

--- js/sprint5/E/code.js
+++ js/sprint5/E/code.js
@@ -7,9 +7,21 @@ if (process.env.REMOTE_JUDGE !== 'true') {
         }
     }
 }
+// https://contest.yandex.ru/contest/24809/run-report/142010795/
 
-function solution(root) {
+function solution(root, min = -Infinity, max = +Infinity) {
     // Your code
+    if (root === null) {
+        return true
+    }
+
+    let valueBiggerThanRight = root.value >= max;
+    let valueSmallerThanLeft = root.value <= min;
+    if (valueBiggerThanRight || valueSmallerThanLeft) {
+        return false
+    }
+
+    return solution(root.left, min, root.value) && solution(root.right, root.value, max)
     // “ヽ(´▽｀)ノ”
 }
 
@@ -18,6 +30,7 @@ function test() {
     var node2 = new CNode(4, null, null);
     var node3 = new CNode(3, node1, node2);
     var node4 = new CNode(8, null, null);
+    var node5 = new CNode(5, node3, node4);
     var node5 = new CNode(5, node3, node4);
     console.assert(solution(node5));
     node4.value = 5;
