8d205daf24847324fd81630e838a5a08bd0ae28a
Marat Khusnetdinov <marat@khusnetdinov.ru>
2025-09-16 19:53:34 +0300

    Sprint5:L, M

--- js/sprint5/L/code.js
+++ js/sprint5/L/code.js
@@ -1,5 +1,37 @@
-function siftDown(heap, idx) {
+
+// https://contest.yandex.ru/contest/24809/run-report/142424415/
+function siftDown(heap, index) {
     // Your code
+    const size = heap.length - 1;
+    const getLeftIndex = (index) => { return 2 * index };
+    const getRightIndex = (index) => { return 2 * index + 1 }
+
+    let currentIndex = index
+    while (true) {
+        let leftChildIndex = getLeftIndex(currentIndex)
+        let rightChildIndex = getRightIndex(currentIndex)
+        let largestIndex = currentIndex
+
+        // console.log(`left = ${ heap[leftChildIndex] } root = ${ heap[currentIndex] } right = ${ heap[rightChildIndex] }`)
+
+        if (leftChildIndex <= size && heap[leftChildIndex] > heap[largestIndex]) {
+            largestIndex = leftChildIndex;
+        }
+
+        if (rightChildIndex <= size && heap[rightChildIndex] > heap[largestIndex]) {
+            largestIndex = rightChildIndex;
+        }
+
+        if (largestIndex === currentIndex) {
+            break;
+        }
+
+        [heap[currentIndex], heap[largestIndex]] = [heap[largestIndex], heap[currentIndex]];
+
+        currentIndex = largestIndex;
+    }
+
+    return currentIndex;
     // “ヽ(´▽｀)ノ”
 }
 
--- js/sprint5/M/code.js
+++ js/sprint5/M/code.js
@@ -1,5 +1,24 @@
-function siftUp(heap, idx) {
+// https://contest.yandex.ru/contest/24809/run-report/142425158/
+function siftUp(heap, index) {
     // Your code
+    if (index === 1) {
+        return index;
+    }
+
+    let currentIndex = index;
+
+    while (currentIndex > 1) {
+        const parentIndex = Math.floor(currentIndex / 2);
+
+        if (heap[currentIndex] > heap[parentIndex]) {
+            [heap[currentIndex], heap[parentIndex]] = [heap[parentIndex], heap[currentIndex]];
+            currentIndex = parentIndex;
+        } else {
+            break;
+        }
+    }
+
+    return currentIndex;
     // “ヽ(´▽｀)ノ”
 }
 
