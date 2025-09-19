821dd7c9f15b8e1cec16a2fa54f52a0c151e984f
Marat Khusnetdinov <marat@khusnetdinov.ru>
2025-09-19 20:23:05 +0300

    Sprint5Final:B

--- js/sprint5_final/B/code.js
+++ js/sprint5_final/B/code.js
@@ -1,16 +1,60 @@
+// https://contest.yandex.ru/contest/24810/run-report/142647630/
+
+/*
+ * -- ПРИНЦИП РАБОТЫ --
+ * 1. Поиск узла: Рекурсивно спускаемся по дереву, сравнивая ключ с текущим значением узла
+ * 2. Три случая удаления:
+ *    - Нет левого потомка: возвращаем правый потомок
+ *    - Нет правого потомка: возвращаем левый потомок
+ *    - Есть оба потомка: находим минимальный узел в правом поддереве, копируем его значение и рекурсивно удаляем его
+ *
+ * Сложность:
+ * - Время: O(h), где h - высота дерева
+ * - Память: O(h) для рекурсивных вызовов
+ */
 if (process.env.REMOTE_JUDGE !== 'true') {
-  class Node {
-      constructor(value, left = null, right = null) {
-          this.value = value;
-          this.left = left;
-          this.right = right;
-      }
-  }
+    class Node {
+        constructor(value, left = null, right = null) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
 
 function remove(node, key) {
-    // Your code
-    // “ヽ(´▽｀)ノ”
+    if (node === null) {
+        return null;
+    }
+
+    if (key < node.value) {
+        node.left = remove(node.left, key);
+    } else if (key > node.value) {
+        node.right = remove(node.right, key);
+    } else {
+        if (node.left === null) {
+            return node.right;
+        } else if (node.right === null) {
+            return node.left;
+        } else {
+            let minNode = findMin(node.right);
+
+            node.value = minNode.value;
+            node.right = remove(node.right, minNode.value);
+        }
+    }
+
+    return node;
+}
+
+function findMin(node) {
+    let current = node;
+
+    while (current.left !== null) {
+        current = current.left;
+    }
+
+    return current;
 }
 
 function test() {
